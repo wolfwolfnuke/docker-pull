a80922173dd8b308bc4a2f3b0458bfdce41fa60a
wolfwolfnuke <97002598+wolfwolfnuke@users.noreply.github.com>
2025-05-28 16:53:50 -0700

    running both ngrok and http-server at the same time

--- launch.sh
+++ launch.sh
@@ -2,11 +2,11 @@ git pull
 cd http-files
 ls
 echo Launching ngrok
-ngrok http --url=remotely-alive-hippo.ngrok-free.app 80 
+ngrok http --url=remotely-alive-hippo.ngrok-free.app 80 && http-server -p 80
 echo Launched ngrok
-echo Launching http-server
-http-server -p 80
-echo Launched http-server
+#echo Launching http-server
+
+#echo Launched http-server
 
 ping 100.108.8.7
 sleep infinity
