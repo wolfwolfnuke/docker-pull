129168668869cadc7b50e62f40c1f4335e3d9c14
wolfwolfnuke <97002598+wolfwolfnuke@users.noreply.github.com>
2025-05-28 16:47:47 -0700

    testing some fixes

--- launch.sh
+++ launch.sh
@@ -1,11 +1,12 @@
 git pull
 cd http-files
 ls
-echo Launching http-server
-http-server -p 80
-echo Launched http-server
 echo Launching ngrok
 ngrok http --url=remotely-alive-hippo.ngrok-free.app 80 
 echo Launched ngrok
+echo Launching http-server
+http-server -p 80
+echo Launched http-server
+
 ping 100.108.8.7
 sleep infinity
