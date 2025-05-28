303d17bbd7f17707f179a2fd7105332a97ca69ad
wolfwolfnuke <97002598+wolfwolfnuke@users.noreply.github.com>
2025-05-28 16:55:25 -0700

    reverted

--- launch.sh
+++ launch.sh
@@ -4,9 +4,9 @@ ls
 echo Launching ngrok
 ngrok http --url=remotely-alive-hippo.ngrok-free.app 80 && http-server -p 80
 echo Launched ngrok
-#echo Launching http-server
-
-#echo Launched http-server
+echo Launching http-server
+http-server -p 80
+echo Launched http-server
 
 ping 100.108.8.7
 sleep infinity
